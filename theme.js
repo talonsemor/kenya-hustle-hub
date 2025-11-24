78e6fdc834fb0fc1ff2faae7d441d49834374a79
talonsemor <shadrackechesa40@gmail.com>
2025-11-24 07:08:05 +0300

    Delete theme.js

--- theme.js
+++
@@ -1,5 +0,0 @@
-
-const themeBtn = document.getElementById('themeToggle');
-const mpesaNum = document.getElementById('mpesaNum');
-if(themeBtn){ themeBtn.addEventListener('click', ()=>{ document.body.classList.toggle('light'); document.body.classList.toggle('dark'); }); }
-if(mpesaNum){ mpesaNum.textContent = '0740582544'; }
